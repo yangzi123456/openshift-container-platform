61b8e7b88d68744a699a95a701c6a4082d87d7fa
掌柜的 <danielwzhg@gmail.com>
2018-08-17 08:42:45 +0800

    Update masterPrep.sh

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -94,8 +94,9 @@ yum -y install docker
 # Update docker storage
 echo "
 # Adding insecure-registry option required by OpenShift
-OPTIONS=\"\$OPTIONS --insecure-registry 172.30.0.0/16\"
+# OPTIONS=\"\$OPTIONS --insecure-registry 172.30.0.0/16\"
 " >> /etc/sysconfig/docker
+
 
 # Create thin pool logical volume for Docker
 echo $(date) " - Creating thin pool logical volume for Docker and staring service"
@@ -119,7 +120,7 @@ else
 fi
 
 # Enable and start Docker services
-
+sudo sed -i -e "s/^OPTIONS=' --selinux-enabled/OPTIONS='--selinux-enabled=False/" /etc/sysconfig/docker
 systemctl enable docker
 systemctl start docker
 
